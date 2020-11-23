9363250b82f042639a6f7e9bbfb5eceae984535e
charifh-noureldin <nour.charifh.001@student.uni.lu>
2020-11-23 07:23:41 +0100

    improve the array

--- wordsClass.py
+++ wordsClass.py
@@ -1,2 +1,5 @@
 class wordsClass:
-    words = ['the', 'he', 'she']
+    words = ['the','and', 'he', 'she', 'it', 'we', 'they', 'to', 'am', 'is', 'are', 'were', 'was', 'us', 'them', 
+    'on', 'in', 'of', 'by', 'for', 'have', 'has', 'had', 'an', 'its', 'this', 'from', 'will', 'that', 'these',
+    'be', 'been', 'as', 'with', 'can', 'could', 'would', 'about', 'into', 'at', "it's", "i'm", 'no', 'yes','than',
+    ]
